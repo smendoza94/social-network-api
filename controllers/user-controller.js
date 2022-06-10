4291bcbb443b8106ac2d66f48ce7579fb7fa2c9a
StevenM <stevenbmendoza@gmail.com>
2022-06-09 22:09:33 -0700

    controller skeleton, user get all function

---
+++ controllers/user-controller.js
@@ -0,0 +1,13 @@
+const { User } = require("../models");
+
+const userController = {
+  // get all users
+  getAllUsers(req, res) {
+    User.find({})
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+};
